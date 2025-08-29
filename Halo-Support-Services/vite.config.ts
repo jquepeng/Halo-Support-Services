ac422850ab3c58c36a2c9b1c575e0dc261ddaf5a
Jeremy Ng <jeremy.nathan.ng@gmail.com>
2025-08-29 16:30:20 +0800

    Added base to vite config

--- Halo-Support-Services/vite.config.ts
+++ Halo-Support-Services/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
+    base: "/Halo-Support-Services",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 })
