be9885b613fcfc0b36d3b1e8b3af46afa7e26d9b
Jeremy Ng <jeremy.nathan.ng@gmail.com>
2025-09-12 13:22:47 +0800

    Changed layout of services page and added basic flavor animation

--- src/components/Service-option-card.tsx
+++ src/components/Service-option-card.tsx
@@ -1,32 +1,58 @@
-import { Card } from "@/components/ui/card";
+import {Card} from "@/components/ui/card";
+import {ChevronsRight} from "lucide-react";
 import {Button} from "@/components/ui/button.tsx";
+import {cn} from "@/lib/utils.ts";
+import {Separator} from "@/components/ui/separator.tsx";
 
 type ServiceOptionCardProps = {
-  name: string;
-  description: string;
-  imagePath: string;
-  url: string;
-  reversed?: boolean;
+    name: string;
+    description: string;
+    imagePath: string;
+    url: string;
+    reversed?: boolean;
 };
 
-const ServiceOptionCard = ({ name, description, imagePath, url, reversed=false }: ServiceOptionCardProps) => {
-  return (
-    <Card className="p-4 max-w-4xl bg-white">
-      <div className={`flex ${reversed && 'md:flex-row-reverse'} flex-col sm:flex-row w-max-92 sm:w-fit gap-4`}>
-        <div className={`flex-1 flex min-w-64 flex-col`}>
-          <h1 className="text-4xl text-primary font-bold uppercase">{name}</h1>
-          <p className="mt-4 text-primary uppercase">{description}</p>
-        </div>
-        <div className="bg-black w-max-92 sm:w-64 h-64 flex-shrink-0 overflow-hidden relative">
-          <div className={"absolute flex flex-col w-full h-full p-4 justify-end z-10"} >
-            <h3 className={"text-white text-xl text-shadow-2xs font-bold uppercase"}>{name}</h3>
-            <Button className={"rounded-full w-max font-light uppercase"} onClick={()=>url}>Read more</Button>
-          </div>
-          <img src={imagePath} alt={name} className="object-cover w-full h-full absolute" />
-        </div>
-      </div>
-    </Card>
-  );
+const ServiceOptionCard = ({name, description, imagePath, url, reversed = false}: ServiceOptionCardProps) => {
+    return (
+        <Card className="p-4 max-w-4xl w-full bg-white">
+            <div
+                className={cn(
+                    "flex flex-col sm:flex-row gap-4 w-full",
+                    reversed && "md:flex-row-reverse"
+                )}
+            >
+                {/* Text content */}
+                <div className="flex-1 flex flex-col">
+                    <div className={"w-fit"}>
+                        <h1 className="text-4xl text-primary font-bold uppercase">{name}</h1>
+                        <Separator className={"mb-[2px] data-[orientation=horizontal]:w-4/5"}/>
+                    </div>
+                    <p className="mt-4 text-accent-foreground">{description}</p>
+                </div>
+
+                {/* Image block */}
+                <div className="relative h-64 w-full sm:w-64 rounded-sm flex-shrink-0 bg-slate-700 overflow-hidden">
+                    <div className="absolute inset-0 flex flex-col p-4 justify-end z-10">
+                        <h3 className="text-white text-xl font-bold uppercase text-shadow-lg">
+                            {name}
+                        </h3>
+                        <Button
+                            className="rounded-full w-max font-light uppercase group"
+                            onClick={() => url}
+                        >
+                            Read more <ChevronsRight/>
+                        </Button>
+                    </div>
+                    <img
+                        src={imagePath}
+                        alt={name}
+                        className="absolute inset-0 w-full h-full object-cover"
+                    />
+                </div>
+            </div>
+        </Card>
+
+    );
 };
 
 export default ServiceOptionCard;
--- src/routes/services.tsx
+++ src/routes/services.tsx
@@ -10,31 +10,35 @@ export const Route = createFileRoute('/services')({
 
 function Services() {
     return (
-        <div className="w-full flex flex-col gap-4 items-center pt-nav">
-            <div className={"flex flex-col w-full pt-6"}>
+        <div className="w-full flex flex-col gap-4 items-center pt-nav min-h-screen bg-secondary">
+            <div className={"flex flex-col w-full pt-6 gap-4"}>
                 <Section>
                     <div className="flex flex-col gap-4 items-center">
-                        <h1 className={"text-6xl text-center"}>What can <span
-                            className={"text-transparent bg-clip-text font-semibold bg-linear-120 from-primary to-ndis"}>we</span> do
+                        <h1 className={"text-6xl text-center group"}>What can <span
+                            className={"text-transparent bg-clip-text font-semibold gradient group-hover:animate-gradient"}>we</span> do
                             for <span
-                                className={"text-transparent bg-clip-text font-semibold bg-linear-120 from-primary to-ndis"}>you</span>?
+                                className={"text-transparent bg-clip-text font-semibold gradient group-hover:animate-gradient"}>
+                                you
+                                </span>?
                         </h1>
-                        <div className={"flex flex-col max-w-4xl gap-4 items-center"}>
-                            <Separator/>
-                            <ServiceOptionCard
-                                name={"Support Work"}
-                                description={"I BELIEVE LIAM LOVES BRANDONE SO MUCH HE SAID HE IS GOING TO WORK WITH HIM FOR FREE FROM NOW ON!"}
-                                imagePath={HSSLogo}
-                                url={"test"}
-                            />
-                            <ServiceOptionCard
-                                name={"Support Work"}
-                                description={"I BELIEVE LIAM LOVES BRANDONE SO MUCH HE SAID HE IS GOING TO WORK WITH HIM FOR FREE FROM NOW ON!"}
-                                imagePath={HSSLogo}
-                                url={"test"}
-                                reversed
-                            />
-                        </div>
+                    </div>
+                </Section>
+                <Section className="flex items-center">
+                    <div className={"flex flex-col max-w-4xl gap-4 items-center"}>
+                        <Separator/>
+                        <ServiceOptionCard
+                            name={"Support Work"}
+                            description={"I BELIEVE LIAM LOVES BRANDONE SO MUCH HE SAID HE IS GOING TO WORK WITH HIM FOR FREE FROM NOW ON!"}
+                            imagePath={HSSLogo}
+                            url={"test"}
+                        />
+                        <ServiceOptionCard
+                            name={"Support Work"}
+                            description={"Some sample text that describes what Halo can do for a client yahoo!"}
+                            imagePath={HSSLogo}
+                            url={"test"}
+                            reversed
+                        />
                     </div>
                 </Section>
             </div>
